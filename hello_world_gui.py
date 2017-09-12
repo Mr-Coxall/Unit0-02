be5f2da444ad1c07e7ad8eff41c051bb699351c7
Patrick Coxall <mr.coxall@mths.ca>
2017-09-12 12:59:05 -0400

    Added comment

--- hello_world_gui.py
+++ hello_world_gui.py
@@ -1,3 +1,9 @@
+# Created by: Mr. Coxall
+# Created on: 12-Sep-2017
+# Created for: ICS3U
+# Daily Assignment – Unit0-02
+# This program displays Hello World but in GUI
+
 import ui
 
 v = ui.load_view()
